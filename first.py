f41f163f5a524d88ecf7ea56130e3f146c727632
asw192 <36547681+asw192@users.noreply.github.com>
2024-01-24 11:14:19 -0500

    Add files via upload
    First module assessment commit

---
+++ first.py
@@ -0,0 +1,4 @@
+name='abhijit'
+age=35
+summary='Learn python coding and working of github account'
+print("My name is " + name + " and I am " + str(age) + " years old. " + "And I am here to " + summary)
